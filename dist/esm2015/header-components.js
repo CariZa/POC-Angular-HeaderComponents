b144f0ff70866dd8e9c006ce18cdacf02614d5b6
CariZa <liebenbc@yahoo.com>
2018-11-12 13:01:05 +0200

    Added newly created dist files

---
+++ dist/esm2015/header-components.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { HeaderModule } from './public_api';
+export { BlueHeaderComponent as ɵa } from './src/app/modules/components/blue-header/blue-header.component';
+export { RedHeaderComponent as ɵb } from './src/app/modules/components/red-header/red-header.component';
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaGVhZGVyLWNvbXBvbmVudHMuanMiLCJzb3VyY2VSb290Ijoibmc6Ly9oZWFkZXItY29tcG9uZW50cy8iLCJzb3VyY2VzIjpbImhlYWRlci1jb21wb25lbnRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFJQSw2QkFBYyxjQUFjLENBQUM7QUFFN0IsT0FBTyxFQUFDLG1CQUFtQixJQUFJLEVBQUUsRUFBQyxNQUFNLGdFQUFnRSxDQUFDO0FBQ3pHLE9BQU8sRUFBQyxrQkFBa0IsSUFBSSxFQUFFLEVBQUMsTUFBTSw4REFBOEQsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJhdGVkIGJ1bmRsZSBpbmRleC4gRG8gbm90IGVkaXQuXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9wdWJsaWNfYXBpJztcblxuZXhwb3J0IHtCbHVlSGVhZGVyQ29tcG9uZW50IGFzIMm1YX0gZnJvbSAnLi9zcmMvYXBwL21vZHVsZXMvY29tcG9uZW50cy9ibHVlLWhlYWRlci9ibHVlLWhlYWRlci5jb21wb25lbnQnO1xuZXhwb3J0IHtSZWRIZWFkZXJDb21wb25lbnQgYXMgybVifSBmcm9tICcuL3NyYy9hcHAvbW9kdWxlcy9jb21wb25lbnRzL3JlZC1oZWFkZXIvcmVkLWhlYWRlci5jb21wb25lbnQnOyJdfQ==
